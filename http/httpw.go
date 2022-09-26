55ecaf467c2c90b7765fe165130aec20d6166474
ShadowFl0w <1712472820@qq.com>
2022-09-26 15:21:43 +0800

    update

--- http/httpw.go
+++ http/httpw.go
@@ -21,25 +21,21 @@ func main() {
 	ipPortList := strings.Split(string(content), "\n")
 
 	for _, item := range ipPortList {
-		httpx(item)
+		url := "http://" + item
+		//resp, err := http.Get(url)
+		req, err := http.NewRequest(http.MethodGet, url, nil)
+		cli := http.Client{
+			Timeout: time.Millisecond * 3,
+		}
+		resp, err := cli.Do(req)
+		if err != nil {
+			continue
+		}
+		defer resp.Body.Close()
+		body, err := ioutil.ReadAll(resp.Body)
+		if strings.Contains(string(body), "swagger-ui") {
+			fmt.Println(item)
+		}
 	}
 
-}
-
-func httpx(host string) {
-	url := "http://" + host
-	//resp, err := http.Get(url)
-	req, err := http.NewRequest(http.MethodGet, url, nil)
-	cli := http.Client{
-		Timeout: time.Millisecond * 3,
-	}
-	resp, err := cli.Do(req)
-	if err != nil {
-		log.Panic(err)
-	}
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if strings.Contains(string(body), "swagger-ui") {
-		fmt.Println(host)
-	}
 }
