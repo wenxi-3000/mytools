6c6194db4773af7fe86f96ae8a3581b08eab8f79
ShadowFl0w <1712472820@qq.com>
2022-09-26 15:46:27 +0800

    update

--- http/httpw.go
+++ http/httpw.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"crypto/tls"
 	"fmt"
 	"io/ioutil"
 	"log"
+	"net"
 	"net/http"
 	"os"
 	"strings"
@@ -11,6 +13,7 @@ import (
 )
 
 func main() {
+	//文件处理
 	file, err := os.Open("./ip.txt")
 	if err != nil {
 		log.Panic(err)
@@ -20,22 +23,54 @@ func main() {
 	content, err := ioutil.ReadAll(file)
 	ipPortList := strings.Split(string(content), "\n")
 
+	//http机制
+	timeout := time.Duration(10000 * 1000000)
+	var tr = &http.Transport{
+		MaxIdleConns:      30,
+		IdleConnTimeout:   time.Second,
+		DisableKeepAlives: true,
+		TLSClientConfig:   &tls.Config{InsecureSkipVerify: true},
+		DialContext: (&net.Dialer{
+			Timeout:   timeout,
+			KeepAlive: time.Second,
+		}).DialContext,
+	}
+
+	re := func(req *http.Request, via []*http.Request) error {
+		return http.ErrUseLastResponse
+	}
+
+	client := &http.Client{
+		Transport:     tr,
+		CheckRedirect: re,
+		Timeout:       timeout,
+	}
+
 	for _, item := range ipPortList {
 		url := "http://" + item
 		//resp, err := http.Get(url)
-		req, err := http.NewRequest(http.MethodGet, url, nil)
-		cli := http.Client{
-			Timeout: time.Millisecond * 3,
-		}
-		resp, err := cli.Do(req)
+		req, err := http.NewRequest("GET", url, nil)
 		if err != nil {
-			continue
+			log.Println(err)
+		}
+
+		req.Header.Add("Connection", "close")
+		req.Close = true
+
+		resp, err := client.Do(req)
+		if err != nil {
+			log.Println(err)
 		}
 		defer resp.Body.Close()
-		body, err := ioutil.ReadAll(resp.Body)
-		if strings.Contains(string(body), "swagger-ui") {
-			fmt.Println(item)
+
+		if resp != nil {
+			body, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				log.Println(err)
+			}
+			if strings.Contains(string(body), "swagger-ui") {
+				fmt.Println(item)
+			}
 		}
 	}
-
 }
